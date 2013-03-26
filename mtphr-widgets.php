02793c9b2eccbc5d3dc8bc175737435e6f66bb6f
metaphorcreations <metaphorcreations@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-26 13:51:33 +0000

    Added class check for tmhoauth
    git-svn-id: https://plugins.svn.wordpress.org/mtphr-widgets/trunk@687508 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mtphr-widgets.php
+++ mtphr-widgets.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Metaphor Widgets
 Description: Custom widgets pack. Includes recent posts, recent comments, contact, twitter, social, & navigation widgets.
-Version: 2.0.7
+Version: 2.0.8
 Author: Metaphor Creations
 Author URI: http://www.metaphorcreations.com
 License: GPL2
@@ -45,9 +45,9 @@ if( in_array('mtphr-widgets-pack-1/mtphr-widgets-pack-1.php', $active_plugins) )
 	
 	/**Define Widget Constants */
 	if ( WP_DEBUG ) {
-		define ( 'MTPHR_WIDGETS_VERSION', '2.0.7-'.time() );
+		define ( 'MTPHR_WIDGETS_VERSION', '2.0.8-'.time() );
 	} else {
-		define ( 'MTPHR_WIDGETS_VERSION', '2.0.7' );
+		define ( 'MTPHR_WIDGETS_VERSION', '2.0.8' );
 	}
 	define ( 'MTPHR_WIDGETS_DIR', plugin_dir_path(__FILE__) );
 	define ( 'MTPHR_WIDGETS_URL', plugins_url().'/mtphr-widgets' );
@@ -73,6 +73,9 @@ if( in_array('mtphr-widgets-pack-1/mtphr-widgets-pack-1.php', $active_plugins) )
 	require_once( MTPHR_WIDGETS_DIR.'includes/navigation.php' );
 	require_once( MTPHR_WIDGETS_DIR.'includes/collapse.php' );
 	require_once( MTPHR_WIDGETS_DIR.'includes/settings.php' );
-	require_once( MTPHR_WIDGETS_DIR.'includes/tmhOAuth.php' );
-	require_once( MTPHR_WIDGETS_DIR.'includes/tmhUtilities.php' );
+	
+	if( !class_exists('tmhOAuth') ) {
+		require_once( MTPHR_WIDGETS_DIR.'includes/tmhOAuth.php' );
+		require_once( MTPHR_WIDGETS_DIR.'includes/tmhUtilities.php' );
+	}
 }
