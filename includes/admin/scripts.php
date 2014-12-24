2c566cb8b302fcce114901f1dee2d766a7439c4a
metaphorcreations <metaphorcreations@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-24 17:23:36 +0000

    Quick fix
    git-svn-id: https://plugins.svn.wordpress.org/mtphr-widgets/trunk@1053503 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/admin/scripts.php
+++ includes/admin/scripts.php
@@ -11,22 +11,19 @@ function mtphr_widgets_admin_scripts( $hook ) {
 	// Load the fontastic font
 	wp_register_style( 'mtphr-widgets-font', MTPHR_WIDGETS_URL.'/assets/fontastic/styles.css', false, MTPHR_WIDGETS_VERSION );
   wp_enqueue_style( 'mtphr-widgets-font' );
-
-	if ( $hook == 'widgets.php' ) {
-
-		// Load scipts for the media uploader
-		if(function_exists( 'wp_enqueue_media' )){
-	    wp_enqueue_media();
-		} else {
-	    wp_enqueue_style('thickbox');
-	    wp_enqueue_script('media-upload');
-	    wp_enqueue_script('thickbox');
-		}
-
-		// Load the global widgets jquery
-		wp_register_script( 'mtphr-widgets-admin', MTPHR_WIDGETS_URL.'/assets/js/script-admin.js', array('jquery'), MTPHR_WIDGETS_VERSION );
-	  wp_enqueue_script( 'mtphr-widgets-admin' );
+  
+	// Load scipts for the media uploader
+	if(function_exists( 'wp_enqueue_media' )){
+    wp_enqueue_media();
+	} else {
+    wp_enqueue_style('thickbox');
+    wp_enqueue_script('media-upload');
+    wp_enqueue_script('thickbox');
 	}
+
+	// Load the global widgets jquery
+	wp_register_script( 'mtphr-widgets-admin', MTPHR_WIDGETS_URL.'/assets/js/script-admin.js', array('jquery'), MTPHR_WIDGETS_VERSION );
+  wp_enqueue_script( 'mtphr-widgets-admin' );
 
 	// Load the global widgets stylesheet
 	wp_register_style( 'mtphr-widgets-admin', MTPHR_WIDGETS_URL.'/assets/css/style-admin.css', false, MTPHR_WIDGETS_VERSION );
