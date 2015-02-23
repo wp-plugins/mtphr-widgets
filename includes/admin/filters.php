505fd78eac9648bc2ac74eb4029ccba8e980ff1d
metaphorcreations <metaphorcreations@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-23 19:04:38 +0000

    Added Skype protocol.
    git-svn-id: https://plugins.svn.wordpress.org/mtphr-widgets/trunk@1097470 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/admin/filters.php
+++ includes/admin/filters.php
@@ -34,3 +34,15 @@ function mtphr_widgets_twitter_reset() {
 }
 add_action( 'wp_loaded', 'mtphr_widgets_twitter_reset' );
 
+
+
+/* --------------------------------------------------------- */
+/* !Add skype protocal - 2.1.19 */
+/* --------------------------------------------------------- */
+
+function mtphr_widgets_allow_skype_protocol( $protocols ){
+	$protocols[] = 'skype';
+	return $protocols;
+}
+add_filter( 'kses_allowed_protocols' , 'mtphr_widgets_allow_skype_protocol' );
+
--- mtphr-widgets.php
+++ mtphr-widgets.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Metaphor Widgets
 Description: Custom widgets pack. Includes recent posts, recent comments, contact, twitter, social, & navigation widgets.
-Version: 2.1.18
+Version: 2.1.19
 Author: Metaphor Creations
 Author URI: http://www.metaphorcreations.com
 License: GPL2
@@ -28,7 +28,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 
 /**Define Widget Constants */
-define ( 'MTPHR_WIDGETS_VERSION', '2.1.18' );
+define ( 'MTPHR_WIDGETS_VERSION', '2.1.19' );
 define ( 'MTPHR_WIDGETS_DIR', trailingslashit(plugin_dir_path(__FILE__)) );
 define ( 'MTPHR_WIDGETS_URL', trailingslashit(plugins_url()).'mtphr-widgets/' );
 
@@ -42,6 +42,7 @@ if( is_admin() ) {
 } else {
 	require_once( MTPHR_WIDGETS_DIR.'includes/scripts.php' );
 }
+
 require_once( MTPHR_WIDGETS_DIR.'includes/helpers.php' );
 
 require_once( MTPHR_WIDGETS_DIR.'includes/functions.php' );
